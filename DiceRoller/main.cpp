b8d4de4d9e5481217acf92a532271f7b87faee30
Ajaypal Gill <ajgill@ucdavis.edu>
2018-08-02 17:17:56 -0700

    Dice Roller (Dungeons and Dragons format)
    The input is one or more lines in the format of, for example, 3d6. This means 3 dice are being rolled and each has 6 sides. The number of dice can vary from 1-100 and the number of sides on the dice can vary from 2-100. Returns the value of each roll as well as the sum of the input.
    This is an implementation of one dice input.
    Next will be multiple inputs.

--- DiceRoller/main.cpp
+++ DiceRoller/main.cpp
@@ -7,9 +7,47 @@
 //
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+void diceRoller(int x, int y);
+
+int main() {
+    string input;
+    string delimiter = "d";
+    srand((unsigned int)time(NULL));
+    int diceCount, numSides, numIn;
+    cout << "Please enter number of inputs " << endl;
+    cin >> numIn;
+    cout << "Please input dice rolls in the format of NdM" << endl;
+    cin >> input;
+    string token1 = input.substr(0, input.find(delimiter));
+    string token2 = input.erase(0, input.find(delimiter) + delimiter.length());
+    cout << "diceCount " << token1 << " numSides " << token2 << endl;
+    diceCount = stoi(token1);
+    numSides = stoi(token2);
+    diceRoller(diceCount, numSides);
+
+    
+}
+
+void diceRoller(int diceCount, int numSides){
+    int diceSum = 0;
+    int roll;
+    int rollValues[diceCount];
+    for(int i=0; i<diceCount; i++)
+    {
+        roll = 2 + (rand() % (numSides - 1));
+        rollValues[i] = roll;
+        diceSum += roll;
+    }
+    cout << diceSum << " : ";
+    for(int i=0; i<diceCount; i++)
+    {
+        cout << rollValues[i] << " ";
+    }
+    cout << "\n";
 }
